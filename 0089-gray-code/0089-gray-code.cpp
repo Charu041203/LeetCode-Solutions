0270e924a6ace041e51c15a06803a7fec456deeb
Charu041203 <charusinght@gmail.com>
2026-05-19 22:50:23 +0530

    Time: 0 ms (100%), Space: 13.8 MB (98.85%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        int totalNumbers = 1 << n;
+        result.reserve(totalNumbers);
+        
+        for (int i = 0; i < totalNumbers; ++i) {
+            result.push_back(i ^ (i >> 1));
+        }
+        
+        return result;
+    }
+};
