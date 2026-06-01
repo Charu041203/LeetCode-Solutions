026102e2d05ad663dbdf6843090d6daae7092c26
Charu041203 <charusinght@gmail.com>
2026-06-01 22:02:18 +0530

    Time: 4 ms (74.18%), Space: 21.9 MB (14.32%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        unordered_map<long long, int> prefixSums;
+        prefixSums[0] = 1;
+        return dfs(root, 0, targetSum, prefixSums);
+    }
+
+private:
+    int dfs(TreeNode* node, long long currentSum, int targetSum, unordered_map<long long, int>& prefixSums) {
+        if (node == nullptr) {
+            return 0;
+        }
+
+        currentSum += node->val;
+        int count = prefixSums[currentSum - targetSum];
+
+        prefixSums[currentSum]++;
+        
+        count += dfs(node->left, currentSum, targetSum, prefixSums);
+        count += dfs(node->right, currentSum, targetSum, prefixSums);
+        
+        prefixSums[currentSum]--;
+        
+        return count;
+    }
+};
